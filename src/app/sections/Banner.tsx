79820b014429cc9981e960b64c75a04eedbc12d1
dibyamaharzan26 <dibyamaharzan26@gmail.com>
2026-03-30 16:02:07 +0545

    Changes

---
+++ src/app/sections/Banner.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Banner = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Banner
