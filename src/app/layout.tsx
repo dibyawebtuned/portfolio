c5d510a644067ce346bfaa5f4be48b36ac875971
dibyamaharzan26 <dibyamaharzan26@gmail.com>
2026-04-03 16:28:04 +0545

    Cahnges

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
       </head>
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#000000]`}
       >
         {/* <Preloader /> */}
         <ScrollProvider>
